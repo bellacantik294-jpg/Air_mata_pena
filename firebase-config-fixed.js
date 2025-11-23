60bba1c933fefdda2e67c14869bc5be2129672d8
bellacantik294-jpg <bellacantik294@gmail.com>
2025-11-24 05:41:58 +0700

    Add files via upload

---
+++ firebase-config-fixed.js
@@ -0,0 +1,17 @@
+// ===============================
+// Firebase Config v8 - Air Mata Pena
+// ===============================
+
+var firebaseConfig = {
+  apiKey: "AIzaSyD2-BRqOnFX6o4WDl-ZmYs3M6uuk7srYaM",
+  authDomain: "tintaku-58bcf.firebaseapp.com",
+  projectId: "tintaku-58bcf",
+  storageBucket: "tintaku-58bcf.appspot.com",
+  messagingSenderId: "597580314120",
+  appId: "1:597580314120:web:0a3f9ef923d3df22b183bd",
+  measurementId: "G-TVFEBKGMD0"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+console.log("Firebase berhasil diinisialisasi (v8)");
